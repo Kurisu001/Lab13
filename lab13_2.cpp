84f868d29e5ddc59926e44783b430445f6f16507
Kittipong <anonymous51124@gamil.com>
2024-12-27 22:06:06 +0700

    lab13 2 complete

--- lab13_2.cpp
+++ lab13_2.cpp
@@ -21,3 +21,31 @@ int main()
     }while(s != 0 || x != 0 || y != 0);
     return 0;
 }
+
+void showImage(const bool pixel[][M]){
+    for(int i = 0; i<M+2; i++) cout << "-";
+    cout << endl;
+    for(int i=0; i< N; i++){
+        cout << "|";
+
+        for(int j=0; j<M; j++){
+            if( pixel[i][j] == 1) cout << "*";
+            else cout << " ";
+        }
+
+        cout << "|"<< endl;
+    }
+    for(int i = 0; i<M+2; i++) cout << "-";
+    cout << endl;
+}
+
+void updateImage(bool pixel[][M],int s,int x,int y){
+    double check;
+    for (int i = 0 ; i < N; i++){
+        for (int j = 0 ;j<M; j++){
+            check = sqrt(pow((i-x),2)+pow((j-y),2));
+            if (check <= (s-1)) pixel[i][j]=1;
+        }
+    }
+
+}
